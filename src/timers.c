48da40658ba02e2566f4cdd5c3c87395c926d829
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2014-12-11 13:59:07 +0100

    Add restructured blinky

---
+++ src/timers.c
@@ -0,0 +1,13 @@
+#include "timers.h"
+
+/**
+ * Initialize timer 2
+ */
+void initializeTimer2(int counts) {
+	TMR2CN = 0x00;    // Stop Timer 2 and clear TF2
+	CKCON &= ~0x60;	  // Timer 2 clocked based on T2XCLK
+	TMR2RL = -counts; // Initialize reload values
+	TMR2 = 0xffff;  // Set to reload immediately
+	ET2 = 1;       // Enable Timer 2 interrupts
+	TR2 = 1;       // Start Timer 2
+}
