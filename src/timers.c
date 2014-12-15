b5d01b3fa212825bef692f53b28e7845c330b9b5
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2014-12-15 11:24:23 +0100

    Indent

--- src/timers.c
+++ src/timers.c
@@ -6,8 +6,8 @@
 void initializeTimer2(int counts) {
 	TMR2CN = 0x00;    // Stop Timer 2 and clear TF2
 	CKCON &= ~0x60;	  // Timer 2 clocked based on T2XCLK
-	TMR2RL = -counts; // Initialize reload values
-	TMR2 = 0xffff;  // Set to reload immediately
-	ET2 = 1;       // Enable Timer 2 interrupts
-	TR2 = 1;       // Start Timer 2
+	TMR2RL = -counts; // Initialize reload value
+	TMR2 = 0xffff;    // Set to reload immediately
+	ET2 = 1;          // Enable Timer 2 interrupts
+	TR2 = 1;          // Start Timer 2
 }
