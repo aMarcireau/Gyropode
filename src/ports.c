fc0f1f93177cc0249ccc64249bde7dbb29b05bf0
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2014-12-15 11:22:27 +0100

    Reorganize ports definitions

--- src/ports.c
+++ src/ports.c
@@ -4,10 +4,10 @@
  * Initialize ports
  */
 void initializePorts(void) {
-	XBR0 	 = 0x05;
-	XBR1     = 0x42;    //XBARE=1,CEX0, CEX1 routed to Port pins. 
+	XBR0 = 0x05;
+	XBR1 = 0x42;
+	P0SKIP = 0xcc;
 	P0MDOUT |= 0x10;
+	P1MDOUT |= 0x03;
 	P3MDOUT |= 0x08;
-	P0SKIP 	 = 0xcc;
-	P1MDOUT |= 0x03;   // P1.0=1; 1.1=1
 }
